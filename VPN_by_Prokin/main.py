4009401ce8d9a2204fa077c989289f56edba0ef9
Oleg Prokin <prokin.rt@gmail.com>
2024-06-09 21:50:40 +0700

    Обновление 1.4.0

--- VPN_by_Prokin/main.py
+++ VPN_by_Prokin/main.py
@@ -295,10 +295,10 @@ def status(message):
                     traffic = "нет ключа"
                 if user[4] == 0:
                     active_count += 1
-                    active = active + f"{user[3]}, {user[0]}, {user[1]}, баланс: {user[2]} руб. {traffic}\n"
+                    active = active + f"{user[3]}, {user[0]}, {user[1]}, баланс: {user[2]} руб.\n"
                 else:
                     disabled_count += 1
-                    disabled_users = disabled_users + f"{user[3]}, {user[0]}, {user[1]}, баланс: {user[2]} руб. {traffic}\n"
+                    disabled_users = disabled_users + f"{user[3]}, {user[0]}, {user[1]}, баланс: {user[2]} руб.\n"
 
             message_with_users = f"""Активные пользователи: {active_count}\n{active}\nЗаблокированные пользователи: {disabled_count}\n{disabled_users}"""
             bot.send_message(sender_telegram_id, message_with_users, parse_mode="MARKDOWN")
