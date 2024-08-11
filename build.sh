5c40a8dc66ca36474e8aab826e51de39a91c8647
Oleg Prokin <prokin.rt@gmail.com>
2024-08-11 17:08:25 +0700

    Update build script

--- build.sh
+++ build.sh
@@ -11,11 +11,6 @@ rm -rf ./dist
 pyinstaller --paths . \
             --name vpn-bot-develop \
             --onefile main.py
-# build API
-
-pyinstaller --paths . \
-            --name vpn-bot-develop \
-            --onefile main.py
 
 DATE=`date +"%Y-%m-%d_%H:%M:%S"`
 cp dist/vpn-bot-develop /builds/develop/vpn-bot-develop-${DATE}
