4530598b052e137ccdabfc793f96196106b9af5a
Oleg Prokin <prokin.rt@gmail.com>
2024-09-08 15:58:30 +0700

    2.1.0

--- vpn_bot/main.py
+++ vpn_bot/main.py
@@ -447,9 +447,8 @@ def status(message):
             user_stats = dbcon.get_users_stats()
             message = "```txt\n"
             for user in user_stats:
-                logger.info(user[3])
-                traffic = DataConvert.convert_size(int(user[3]))
-                message += str(user[0]) + " " + str(user[1]) + " " + str(user[2]) + " " + str(traffic) + "\n"
+                traffic = DataConvert.convert_size(int(user[4]))
+                message += str(user[0]) + " " + str(user[1]) + " " + str(user[2]) + " " + str(user[3]) + " " + str(traffic) + "\n"
             message += "\n```"
             bot.send_message(sender_telegram_id, f"За последние сутки обработано {connection_count} коннектов")
             bot.send_message(sender_telegram_id, message, parse_mode="MARKDOWN")
